10ec3ad9a87ca349a1a82dfc296df490b7638a7c
Laitooo San <killuasan25@gmail.com>
2021-12-25 16:33:27 -0800

    added new script for zombie movement

---
+++ Assets/scripts/Zombie.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    public float defaultSpeed = 5f;
+    public int defaultHealth = 20;
+    [HideInInspector]
+    public int health;
+    [HideInInspector]
+    public float speed;
+    [HideInInspector]
+    public float slownessTimer = 0f;
+
+    void Start() {
+        health = defaultHealth;
+        speed = defaultSpeed;
+    }
+
+    public void takeDamage(int amount) {
+        health -= amount;
+        if (health < 1) {
+            die();
+        }
+    }
+
+    void die() {
+        // TODO: remove later
+        MoneyManager.instance.gainMoney(10);
+        Destroy(gameObject);
+    }
+
+    public void slowAndDamge(int amount, float slownessTime) {
+        takeDamage(amount);
+        if (health > 0) {
+            speed = defaultSpeed / 2;
+            slownessTimer = slownessTime;
+        }
+    }
+}
